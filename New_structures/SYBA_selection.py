f3dc83c0596c2a2e27b081e8ea82d22e8e47cfa2
XDamianX-coder <52101130+XDamianX-coder@users.noreply.github.com>
2024-01-18 18:42:31 +0100

    selectivity indexes recalculation after issue correction

--- New_structures/SYBA_selection.py
+++ New_structures/SYBA_selection.py
@@ -64,25 +64,25 @@ if __name__ == "__main__":
 
     try:
         #A549
-        model = joblib.load('../Activity/Random_forest/random_forest_model_2_estimators_A549.joblib')
+        model = joblib.load('../Activity/Random_forest/random_forest_model_17_estimators_A549.joblib')
         mob = model.predict(molecular_descriptors[list(model.feature_names_in_)])
         converted = pred_model.inverse_transform(mob)
         df_gen_fin['A549 [nM]'] = converted
         
         #BALB/3T3
-        model = joblib.load('../Activity/Random_forest/random_forest_model_2_estimators_BALB_3T3.joblib')
+        model = joblib.load('../Activity/Random_forest/random_forest_model_19_estimators_BALB_3T3.joblib')
         mob = model.predict(molecular_descriptors[list(model.feature_names_in_)])
         converted = pred_model.inverse_transform(mob)
         df_gen_fin['BALB/3T3 [nM]'] = converted
 
         #LoVo
-        model = joblib.load('../Activity/Random_forest/random_forest_model_3_estimators_LoVo.joblib')
+        model = joblib.load('../Activity/Random_forest/random_forest_model_18_estimators_LoVo.joblib')
         mob = model.predict(molecular_descriptors[list(model.feature_names_in_)])
         converted = pred_model.inverse_transform(mob)
         df_gen_fin['LoVo [nM]'] = converted
 
         #LoVo/DX
-        model = joblib.load('../Activity/Random_forest/random_forest_model_8_estimators_LoVo_DX.joblib')
+        model = joblib.load('../Activity/Random_forest/random_forest_model_14_estimators_LoVo_DX.joblib')
         mob = model.predict(molecular_descriptors[list(model.feature_names_in_)])
         converted = pred_model.inverse_transform(mob)
         df_gen_fin['LoVo/DX [nM]'] = converted
